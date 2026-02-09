d1d4d7fc9df62983340a30f8e31db1fc5c0f93b0
shubh <shubhamsharmajcc@gmail.com>
2026-02-09 13:27:18 +0530

    solved

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,25 @@
 
-import React from "react";
+import React, { useEffect, useState } from "react";
 import './../styles/App.css';
 
 const App = () => {
+  const [name, setName] = useState('');
+  const [greeting, setGreeting] = useState('');
+
+  
+    useEffect(() => {
+      if(name){
+        setGreeting(`Hello, ${name}!`);
+      }
+    }, [name]);
+  
   return (
     <div>
-        {/* Do not remove the main div */}
+      {/* Do not remove the main div */}
+      <h1>Enter your name:</h1>
+      <input type="text" value={name} onChange={(event) => setName(event.target.value)} />
+
+      {greeting}
     </div>
   )
 }
